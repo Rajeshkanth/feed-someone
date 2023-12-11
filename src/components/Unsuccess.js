027de642b58df09ebf219a6d9e75abf9c33fde5c
Rajesh <rajesh@Admins-MacBook-Pro.local>
2023-12-11 13:33:49 +0530

    last on 11

--- src/components/Unsuccess.js
+++ src/components/Unsuccess.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { RiFileWarningLine } from "react-icons/ri";
 function Unsuccess() {
   const [sec, setSec] = useState(3);
 
@@ -13,6 +13,9 @@ function Unsuccess() {
 
   return (
     <div className="unsuccess">
+      <div className="error">
+        <RiFileWarningLine />
+      </div>
       <h6>
         Donation process has been cancelled. Your donation was not processed.
         <p>You will be redirecting to the home page in {sec} seconds.</p>
